cbdbd3e1ece2dcc28f262bbd3f0897b098b76178
zzl <1819011603@qq.com>
2021-08-16 12:30:50 +0800

    Adaptee子类 用来说明对象适配器与类适配器的差别

---
+++ src/com/company/adapter/ExtendAdaptee.java
@@ -0,0 +1,12 @@
+package com.company.adapter;
+
+/**
+ * @author 18190
+ * @Date: 2021/8/16  12:05
+ * @VERSION 1.0
+ *
+ * Adaptee子类 用来说明对象适配器与类适配器的差别
+ */
+public class ExtendAdaptee extends Adaptee{
+
+}
